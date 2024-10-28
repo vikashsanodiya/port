ae033ca3fc249c84e303b96f0f0a62f2255dc0cb
vikas-011 <vikas.tech011@gmail.com>
2024-10-28 23:53:16 +0530

    sumin

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -3,52 +3,50 @@ import bcryptjs from "bcryptjs";
 import { generateToken } from "@/lib/auth";
 import connectDB from "@/config/db";
 import Admin from "@/models/admin";
+
 connectDB();
 
 export async function POST(req: NextRequest) {
   try {
     const reqBody = await req.json();
-    const { username, password } = await reqBody;
-    const user = await Admin.findOne({ username });
+    const { email, password } = reqBody; // Changed from username to email
+    const user = await Admin.findOne({ email });
+    
     if (!user) {
       return NextResponse.json(
-        {
-          message: "User not found",
-          success: false,
-        },
+        { message: "User not found", success: false },
         { status: 404 }
       );
     }
+    
     const isValid = await bcryptjs.compare(password, user.password);
     if (!isValid) {
       return NextResponse.json(
-        {
-          message: "Invalid Credentials",
-          success: false,
-        },
+        { message: "Invalid Credentials", success: false },
         { status: 401 }
       );
     }
+    
     const token = await generateToken(
-      { username },
+      { email },
       process.env.ACCESS_SECRET_KEY!
     );
+    
     const res = NextResponse.json({
       message: "Logged in Successfully",
       success: true,
     });
-    res.cookies.set("adminToken", token.toString(), {
+    
+    res.cookies.set("adminToken", token, {
       httpOnly: true,
       secure: true,
       maxAge: 3600000,
     });
+    
     return res;
   } catch (error: any) {
     return NextResponse.json(
-      {
-        message: error.message,
-        success: false,
-      },
+      { message: error.message, success: false },
       { status: 500 }
     );
   }
--- src/middleware.ts
+++ src/middleware.ts
@@ -4,31 +4,40 @@ import { verifyToken } from "./lib/auth";
 
 export async function middleware(request: NextRequest) {
   const authToken = request.cookies.get("adminToken")?.value;
-  const { error } = await verifyToken(
-    authToken!,
-    process.env.ACCESS_SECRET_KEY!
-  );
-  if (error) {
-    if (
-      request.nextUrl.pathname.startsWith("/api/projects/cud") ||
-      request.nextUrl.pathname.startsWith("/api/skills/cud") ||
-      request.nextUrl.pathname.startsWith("/api/contact/rd")
-    ) {
-      return NextResponse.json(
-        {
-          message: "User is not authorized",
-          success: false,
-        },
-        { status: 401 }
-      );
-    }
-    if (request.nextUrl.pathname.startsWith("/admin")) {
-      return NextResponse.redirect(new URL("/", request.url));
-    }
+  
+  if (!authToken) {
+    return handleUnauthorizedResponse(request);
   }
-  if (request.nextUrl.pathname.startsWith("/login") && !error) {
+
+  const { error } = await verifyToken(authToken, process.env.ACCESS_SECRET_KEY!);
+  
+  if (error) {
+    return handleUnauthorizedResponse(request);
+  }
+  
+  if (request.nextUrl.pathname.startsWith("/login")) {
     return NextResponse.redirect(new URL("/admin", request.url));
   }
+  
+  return NextResponse.next();
+}
+
+function handleUnauthorizedResponse(request: NextRequest) {
+  if (
+    request.nextUrl.pathname.startsWith("/api/projects/cud") ||
+    request.nextUrl.pathname.startsWith("/api/skills/cud") ||
+    request.nextUrl.pathname.startsWith("/api/contact/rd")
+  ) {
+    return NextResponse.json(
+      { message: "User is not authorized", success: false },
+      { status: 401 }
+    );
+  }
+  
+  if (request.nextUrl.pathname.startsWith("/admin")) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+  
   return NextResponse.next();
 }
 
